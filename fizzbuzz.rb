9116277e104fe3a707b0fdc64ef4ef069524aed2
Ben <benlhall584@hotmail.co.uk>
2022-01-14 15:07:21 +0000

    Added Fizz and Buzz statements to Fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,12 @@
   if num % 3 == 0 && num % 5 == 0   # if divisible by 3 AND 5, use "FizzBuzz"
     puts "FizzBuzz"
     is_it_fizzbuzz = true
+  elsif num % 3 == 0
+    puts "Fizz"
+    is_it_fizzbuzz = true
+  elsif num % 5 == 0
+    puts "Buzz"
+    is_it_fizzbuzz = true
   end        
   if !is_it_fizzbuzz                 # if not divisable by 3 or 5, use the number
     puts num
